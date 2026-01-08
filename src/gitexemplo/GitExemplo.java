56bfba06477ff49adb3991906548bcf3b81da77b
“Weverton <ricka7x79@gmail.com>
2026-01-08 13:21:04 -0300

    HELLO WORD

--- src/gitexemplo/GitExemplo.java
+++ src/gitexemplo/GitExemplo.java
@@ -1,20 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package gitexemplo;
 
-/**
- *
- * @author wh_ri
- */
+
 public class GitExemplo {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("HELLO WORD!");
+        
+        
+        
     }
     
 }
